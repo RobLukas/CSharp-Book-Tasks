cfac5e86a4d6e4881f7d417265c0c0c81931a510
RobLukas <rjlukasik@wp.pl>
2017-09-30 01:57:53 +0200

    Implement class Game.cs

---
+++ Rozdz 8 zad 7/Rozdz 8 zad 7/Rozdz 8 zad 7/Game.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rozdz_8_zad_7
+{
+    class Game
+    {
+        private List<Player> players;
+        private Dictionary<Values, Player> books;
+        private Deck stock;
+        private TextBox textBoxOnForm;
+
+        public Game(string playerName, IEnumerable<string> opponentNames, TextBox textBoxOnForm)
+        {
+            Random random = new Random();
+            this.textBoxOnForm = textBoxOnForm;
+            players = new List<Player>();
+            players.Add(new Player(playerName, random, textBoxOnForm));
+            foreach (string player in opponentNames)
+                players.Add(new Player(player, random, textBoxOnForm));
+            books = new Dictionary<Values, Player>();
+            stock = new Deck();
+            Deal();
+            players[0].SortHand();
+        }
+        public void Deal()
+        {
+            stock.Sort();
+            for (int i = 0; i < 5; i++)
+                foreach (Player player in players)
+                    player.TakeCard(stock.Deal());
+            foreach (Player player in players)
+                PullOutBooks(player);
+        }
+        public bool PlayOneRound(int selectedPlayerCard)
+        {
+
+        }
+        public bool PullOutBooks(Player player)
+        {
+
+        }
+        public string DescribeBooks()
+        {
+
+        }
+        public string GetWinnerName()
+        {
+
+        }
+        public IEnumerable<string> GetPlayerCardName() => players[0].GetCardNames();
+        public string DescribePlayerHands()
+        {
+            string description = "";
+            for (int i = 0; i < players.Count; i++)
+            {
+                description += players[i].Name + " ma " + players[i].CardCount;
+                if (players[i].CardCount == 1)
+                    description += " kartę." + Environment.NewLine;
+                else if (players[i].CardCount == 2 || players[i].CardCount == 3)
+                    description += " karty." + Environment.NewLine;
+                else
+                    description += " kart." + Environment.NewLine;
+            }
+            description += "Na kupce pozostało kart: " + stock.Count + Environment.NewLine;
+            return description;
+        }
+    }
+}
--- Rozdz 8 zad 7/Rozdz 8 zad 7/Rozdz 8 zad 7/Player.cs
+++ Rozdz 8 zad 7/Rozdz 8 zad 7/Rozdz 8 zad 7/Player.cs
@@ -17,32 +17,60 @@ namespace Rozdz_8_zad_7
 
         public Player(String name, Random random, TextBox textBoxOnForm)
         {
-
+            this.name = name;
+            this.random = random;
+            this.textBoxOnForm = textBoxOnForm;
+            cards = new Deck(new Card[] { });
+            textBoxOnForm.Text = Name + " dołączył do gry.\r\n";
         }
         public Values GetRandomValue()
         {
-
+            Card randomCard = cards.Peek(random.Next(cards.Count));
+            return randomCard.Value;
+            //sprawdzić
+            //Values randomCard = cards.Peek(random.Next(cards.Count)).Value;
+            //return randomCard;
         }
         public Deck DoYouHaveAny(Values value)
         {
-
+            Deck cardsIHave = cards.PullOutValue(value);
+            textBoxOnForm.Text += Name + " ma " + cardsIHave.Count + " " + Card.Plural(value, cardsIHave.Count) + Environment.NewLine;
+            return cardsIHave;
         }
         public void AskForACard(List<Player> players, int myIndex, Deck stock)
         {
-
+            Values randomValue = GetRandomValue();
+            AskForACard(players, myIndex, stock, randomValue);
         }
         public void AskForACard(List<Player> players, int myIndex, Deck stock, Values value)
         {
-
+            textBoxOnForm.Text += Name + " pyta, czy ktoś ma " + Card.Plural(value, 1) + Environment.NewLine;
+            int totalCardsGiven = 0;
+            for (int i = 0; i < players.Count; i++)
+            {
+                if (i != myIndex)
+                {
+                    Player player = players[i];
+                    Deck CardGiven = player.DoYouHaveAny(value);
+                    totalCardsGiven += CardGiven.Count;
+                    while (CardGiven.Count > 0)
+                        TakeCard(CardGiven.Deal());
+                }
+            }
+            if (totalCardsGiven == 0)
+            {
+                TakeCard(stock.Deal());
+                textBoxOnForm.Text += Name + " pobrał kartę z kupki" + Environment.NewLine;
+            }
         }
-        public IEnumerable<Values> PullOutBook()
+        public IEnumerable<Values> PullOutBooks()
         {
             List<Values> books = new List<Values>();
             for (int i = 1; i <= 13; i++)
             {
                 Values value = (Values)i;
                 int howMany = 0;
-                for (int card = 0; card < cards.Count; card++)
+                for (int card = 0; card < CardCount; card++)
                 {
                     if (cards.Peek(card).Value == value)
                         howMany++;
@@ -50,7 +78,7 @@ namespace Rozdz_8_zad_7
                 if (howMany == 4)
                 {
                     books.Add(value);
-                    for (int card = cards.Count - 1; card >= 0; card++)
+                    for (int card = CardCount - 1; card >= 0; card++)
                         cards.Deal(card);
                 }
             }
@@ -62,7 +90,5 @@ namespace Rozdz_8_zad_7
         public IEnumerable<string> GetCardNames() => cards.GetCardNames();
         public Card Peek(int cardNumber) => cards.Peek(cardNumber);
         public void SortHand() { cards.SortByValue(); }
-
-
     }
 }
